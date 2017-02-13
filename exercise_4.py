67ffbd79b817019c8a25e3d4e4a5aea20782ef13
nick davis <nickdavisa@gmail.com>
2017-02-12 22:39:02 -0700

    changed list creation, added comments

--- exercise_4.py
+++ exercise_4.py
@@ -1,8 +1,16 @@
+#don't forget to convert user_num to int here
 user_num = int(input("Please eneter a number: "))
-a = range(1, user_num)
+#new technique for creating lists here.
+a = list(range(1, user_num + 1))
+#empty list to hold divisor values
 b = []
 
+#printing list here to make sure it populates correctly
+print(a)
+
+#loop through list a, check if user_num divides evenly, populate list b
 for num in a:
 	if user_num % num == 0:
 		b.append(num)
+#print list b for verification
 print(b)
