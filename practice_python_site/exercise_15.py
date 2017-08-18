87f4b15524ea46a9f3cbc62077837e9c95ec42ff
Nick Davis <nickdavisa@gmail.com>
2017-08-17 22:49:13 -0700

    Delete exercise_15.py

--- practice_python_site/exercise_15.py
+++
@@ -1,7 +0,0 @@
-def reverse_string(string):
-	new_string = string.split()
-	return " ".join(new_string[::-1])
-
-user_string = input("Please enter a sentance to be reversed: ")
-
-print(reverse_string(user_string))
